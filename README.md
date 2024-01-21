8b6cb8e483d8e78cacd3f0e39eb5caa43aaeddd1
yehuohan <yehuohan@qq.com>
2024-01-21 11:26:19 +0800

    Add symbols

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # cmp-im-zh
 
-用于[cmp-im](https://github.com/yehuohan/cmp-im)的中文码表:
+用于[cmp-im](https://github.com/yehuohan/cmp-im)的中文码表：
 
 - wubi: 86五笔
 - pinyin: 拼音
@@ -9,6 +9,22 @@
 require('cmp_im').setup{
   tables = require('cmp_im_zh').tables{ 'wubi', 'pinyin' }
 }
+```
+
+Map标点符号（对于`括号`，`引号`这些，可能会与autopair之类的插件冲突，需要自行临时disable）：
+
+```lua
+for lhs, rhs in pairs(require('cmp_im_zh').symbols()) do
+    vim.keymap.map('i', lhs, rhs)
+end
+```
+
+Unmap标点符号：
+
+```lua
+for lhs, rhs in pairs(require('cmp_im_zh').symbols()) do
+    vim.keymap.del('i', lhs)
+end
 ```
 
 Much thanks for tables from [ZFVimIM#db-samples](https://github.com/ZSaberLv0/ZFVimIM#db-samples).
--- lua/cmp_im_zh/init.lua
+++ lua/cmp_im_zh/init.lua
@@ -1,11 +1,38 @@
+local function tables(names)
+	local dir = vim.fs.dirname(vim.api.nvim_get_runtime_file("lua/cmp_im_zh/init.lua", false)[1])
+	dir = vim.fs.dirname(vim.fs.dirname(dir))
+	local tbls = {}
+	for _, n in ipairs(names) do
+		tbls[#tbls + 1] = string.format("%s/tables/%s.txt", dir, n)
+	end
+	return tbls
+end
+
+local function symbols()
+	return {
+		["`"] = "·",
+		["!"] = "！",
+		["$"] = "￥",
+		["^"] = "……",
+		["("] = "（",
+		[")"] = "）",
+		["["] = "【",
+		["]"] = "】",
+		["\\"] = "、",
+		[":"] = "：",
+		[";"] = "；",
+		["'"] = "‘’<Left>", -- As auto pair
+		['"'] = "“”<Left>", -- As auto pair
+		[","] = "，",
+		["."] = "。",
+		["<"] = "《",
+		[">"] = "》",
+		["?"] = "？",
+		["_"] = "——",
+	}
+end
+
 return {
-    tables = function(names)
-        local dir = vim.fs.dirname(vim.api.nvim_get_runtime_file('lua/cmp_im_zh/init.lua', false)[1])
-        dir = vim.fs.dirname(vim.fs.dirname(dir))
-        local tbls = { }
-        for _,n in ipairs(names) do
-            tbls[#tbls+1] = string.format('%s/tables/%s.txt', dir, n)
-        end
-        return tbls
-    end
+	tables = tables,
+	symbols = symbols,
 }
